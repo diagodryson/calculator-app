a89927120a0f6c7f23e7dc3917d68ca5b18df969
diagodryson <cortezdiago@gmail.com>
2022-05-27 23:54:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 //DOM
 const touches = document.querySelectorAll('.bouton');
 const listeKeycode = (touches.map => touches.dataset.key);
-console.log(listeKeycode)
+// console.log(listeKeycode)
+
+document.addEventListener('keydown', (e)  => {
+    const valeur = e.keycode.toString();
+
+
+})
+
+document.addEventListener('click' (e)  => {
+    const valeur  = e.target.dataset.key;
+
+    
+})
